c6258a4caf82cec0e166a30c1296b3b7081c3e78
Jordan <40551381+Meschdog18@users.noreply.github.com>
2019-10-16 20:39:58 -0400

    Update Botserver.go

--- Botserver.go
+++ Botserver.go
@@ -21,7 +21,7 @@ func main(){
 }
 func handleRequests(){
 	http.HandleFunc("/", incomingPOST)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func incomingPOST(w http.ResponseWriter, r *http.Request){
 	 var data dat
